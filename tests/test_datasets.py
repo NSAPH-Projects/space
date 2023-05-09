89c583efca5278058580e130e1a8235f3168bc75
naeemkh <khoshnevis.naeem@gmail.com>
2023-05-09 13:43:51 -0400

    return back tests.

---
+++ tests/test_datasets.py
@@ -0,0 +1,13 @@
+import unittest
+
+from spacebench.datasets.datasets import DatasetGenerator
+
+class TestDataGenerator(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(1, 1)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
