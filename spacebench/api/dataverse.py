c97c9e158d7446781ca5ff90b149835a7fe10b82
Ana Trisovic <ana.trisovic@gmail.com>
2023-05-09 19:58:51 -0400

    add docstrings and logger in client

--- spacebench/api/dataverse.py
+++ spacebench/api/dataverse.py
@@ -4,22 +4,27 @@ API for uploading and downloading data from Dataverse.
 import os
 import json
 import tempfile
+
 from pyDataverse.models import Datafile
 from pyDataverse.api import NativeApi, DataAccessApi
+from spacebench.log import LOGGER
 
 
 class DataverseAPI:
+    """
+    A class representing a Dataverse data repository API.
+    """
 
     def __init__(self):
-        self.base_url = 'https://dataverse.harvard.edu/'
-        self.pid = 'doi:10.7910/DVN/SYNPBS'
+        self.base_url = "https://dataverse.harvard.edu/"
+        self.pid = "doi:10.7910/DVN/SYNPBS"
         self.api = NativeApi(self.base_url)
         self.data_api = DataAccessApi(self.base_url)
         self.dataset = self.__get_dataset()
         self.temp_dir = tempfile.gettempdir()
         self.files = {}
         self.data_filename = ""
-    
+
     @property
     def core_data_loc(self):
         """ Returns core data location. """
@@ -55,7 +60,7 @@ class DataverseAPI:
         """ Get fileid from filename for download. """
 
         files_list = self.dataset.json()[
-            'data']['latestVersion']['files']
+            "data"]["latestVersion"]["files"]
 
         for file in files_list:
             filename = file["dataFile"]["filename"]
@@ -64,14 +69,23 @@ class DataverseAPI:
                 self.files[filename] = file["dataFile"]["id"]
 
     def list_data_files(self, include_fileid=False):
-        """ Lists data files from Dataverse. """
+        """
+        Lists data files from Dataverse.
+
+        Args:
+            include_fileid (bool): Include the file ID.
+        """
 
         files_list = self.dataset.json()[
-            'data']['latestVersion']['files']
+            "data"]["latestVersion"]["files"]
         result = []
         for file in files_list:
             file_name = file["dataFile"]["filename"]
-            file_desc = "" #file["dataFile"]["description"]
+            file_desc = ""
+            try:
+                file_desc = file["dataFile"]["description"]
+            except KeyError:
+                pass
             file_id = file["dataFile"]["id"]
             if include_fileid:
                 result.append(f"{file_name}\t{file_desc}\t{file_id}")
@@ -89,8 +103,8 @@ class DataverseAPI:
             )
             if os.path.isfile(file_path):
                 os.remove(file_path)
-                print(
-                    f'{filename} removed from the temporary directory.')
+                LOGGER.info(
+                    f"{filename} removed from the temporary directory.")
 
     def download_data(self, predictor, type):
         """ Downloads core data and dicts from Dataverse. """
@@ -105,28 +119,39 @@ class DataverseAPI:
             )
 
             if os.path.exists(filename_temp_path):
-                print(
-                    f'File {filename} already exists in the temporary directory.')
+                LOGGER.info(
+                    f"File {filename} already exists in the temporary directory.")
             else:
                 response = self.data_api.get_datafile(
                     self.files[filename])
 
-                with open(filename_temp_path, mode='wb') as temp_file:
+                with open(filename_temp_path, mode="wb") as temp_file:
                     temp_file.write(response.content)
                 
-                print("Downloaded: file name {}, id {}".format(
+                LOGGER.info(
+                    "Downloaded: file name {}, id {}".format(
                     filename, self.files[filename]))
 
     def publish_dataset(self, token):
-        """ Publish new dataset. """
+        """
+        Publish new dataset. 
+
+        Args:
+            token (str): Dataverse API Token.
+        """
         api = NativeApi(self.base_url, token)
         resp = api.publish_dataset(self.pid, release_type="major")
         if resp.json()["status"] == "OK":
-           print("Dataset published.")
+           LOGGER.info("Dataset published.")
 
     def upload_data(self, file_path, description, token):
         """
         Upload data to the collection.
+
+        Args:
+            file_path (str): Filename 
+            description (str): Data file description.
+            token (str): Dataverse API Token.
         """
         api = NativeApi(self.base_url, token)
         filename = os.path.basename(file_path)
@@ -137,12 +162,12 @@ class DataverseAPI:
             "filename": filename,
             "description": description,
             })
-        print("File basename: "+filename)
+        LOGGER.info("File basename: "+filename)
         resp = api.upload_datafile(
            self.pid, file_path, dv_datafile.json())
         if resp.json()["status"] == "OK":
-            print("Dataset uploaded.")
-        
+            LOGGER.info("Dataset uploaded.")
+
     def replace(self, file, file_id, token):
         """
         Replaces file in the collection.
@@ -151,14 +176,18 @@ class DataverseAPI:
         filename = os.path.basename(file)
 
         dv_files_list = self.dataset.json()[
-             'data']['latestVersion']['files']
+             "data"]["latestVersion"]["files"]
 
         # keep the description from previous file version
         description = ""
         for dvf in dv_files_list:
-            dv_file_id = dvf['dataFile']['id']
+            dv_file_id = dvf["dataFile"]["id"]
             if str(dv_file_id) == file_id:
-                description = dvf['dataFile']['description']
+                description = ""
+                try:
+                    description = dvf["dataFile"]["description"]
+                except KeyError:
+                    pass
                 break
 
         json_dict = {
@@ -171,9 +200,7 @@ class DataverseAPI:
         json_str = json.dumps(json_dict)
         resp = api.replace_datafile(
             file_id, file, json_str, is_filepid=False)
-        
-        if resp.json()["status"] == "ERROR": 
-            print(resp.content)
-            print("\n")
 
-
+        if resp.json()["status"] == "ERROR":
+            LOGGER.error(
+                f"An error at replacing the file: {resp.content}")
