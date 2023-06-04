287811d54278d95feacf2649890dae65a34e2683
sophi890 <48591088+sophi890@users.noreply.github.com>
2023-06-04 15:46:20 -0400

    adding return args & plots

--- examples/cfs_erf_spatial.py
+++ examples/cfs_erf_spatial.py
@@ -54,34 +54,30 @@ def erf_spatial(dataset:SpaceDataset, pid:int = 0, envname:str = '', filename:st
         counterfactuals_spatial.append(fit_bs_y.predict(dfpred_y, coords))
     counterfactuals_spatial = np.stack(counterfactuals_spatial, axis=1)
 
-    # Create spatialplus cfs
-    fit_bs_x, fit_bs_y = spatialplus.fit(treatment, outcome, coords, df, binary_treatment=False)
-    dfpred_x = pd.DataFrame(covariates,
-                    columns= covnames)
-    xpred = fit_bs_x.predict(dfpred_x, coords)
+
+    fit_bs_y = spatialplus.fit(treatment, outcome, coords, df, binary_treatment=False)[1]
     counterfactuals_spatialplus = []
-    for tval in tvals: # change this!! can do directly.
+    for tval in tvals: 
         dfpred_y = pd.DataFrame(np.column_stack((covariates, np.full_like(dataset.treatment[:, None], tval)
-                                                 -xpred.values.reshape(-1,1))), 
+                                                 -treatment)),
                     columns= covnames + ['r_X'])
         counterfactuals_spatialplus.append(fit_bs_y.predict(dfpred_y, coords))
-        # Alternate method
-        #mu_cf = pd.DataFrame(np.column_stack((covariates, np.full_like(dataset.treatment[:, None], tval))), 
-                   # columns= covnames + ['r_X'])
-        # mu = pd.DataFrame(np.column_stack((covariates, xpred.values.reshape(-1,1))), 
-                   # columns= covnames + ['r_X'])   
-        #ycf = y + (mu_cf - mu)
     counterfactuals_spatialplus = np.stack(counterfactuals_spatialplus, axis=1)
 
     evaluator = DatasetEvaluator(dataset)
     erf_spatial = counterfactuals_spatial.mean(0)
     erf_spatialplus = counterfactuals_spatialplus.mean(0)
-    err_spatial = evaluator.eval(erf=erf_spatial)#, counterfactuals=counterfactuals_spatial)
-    err_spatialplus = evaluator.eval(erf=erf_spatialplus)#, counterfactuals=counterfactuals_spatialplus)
-    erf_error_spatial = err_spatial["erf_av"]
-    erf_error_spatialplus = err_spatialplus["erf_av"]
-    #pehe_spatial = err_spatial["pehe_av"]
-    #pehe_spatialplus = err_spatialplus["pehe_av"]
+    err_spatial_eval = evaluator.eval(erf=erf_spatial, counterfactuals=counterfactuals_spatial)
+    err_spatialplus_eval = evaluator.eval(erf=erf_spatialplus, counterfactuals=counterfactuals_spatialplus)
+    
+    erf_error_spatial = err_spatial_eval["erf_error"]
+    erf_error_spatialplus = err_spatialplus_eval["erf_error"]
+    erf_av_error_spatial = err_spatial_eval["erf_av"]
+    erf_av_error_spatialplus = err_spatialplus_eval["erf_av"]
+    pehe_spatial = err_spatial_eval["pehe_curve"]
+    pehe_spatialplus = err_spatialplus_eval["pehe_curve"]
+    pehe_av_spatial = err_spatial_eval["pehe_av"]
+    pehe_av_spatialplus = err_spatialplus_eval["pehe_av"]
 
 
     smoothness = dataset.smoothness_of_missing
@@ -90,5 +86,21 @@ def erf_spatial(dataset:SpaceDataset, pid:int = 0, envname:str = '', filename:st
     # Write results to file
     with open(filename, 'a') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([envname, pid, smoothness, confounding, erf_error_spatial, erf_error_spatialplus])#, pehe_spatial, pehe_spatialplus]) # can save # dataset as well
+        csvwriter.writerow([envname, 
+                            pid, 
+                            smoothness, 
+                            confounding,
+                            dataset.treatment_values,
+                            dataset.erf(),
+                            erf_spatial,
+                            erf_spatialplus,
+                            erf_error_spatial, 
+                            erf_error_spatialplus, 
+                            erf_av_error_spatial,
+                            erf_av_error_spatialplus,
+                            pehe_spatial, 
+                            pehe_spatialplus,
+                            pehe_av_spatial,
+                            pehe_av_spatialplus
+                            ]) 
     return 
